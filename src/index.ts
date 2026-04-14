7b9ad4508d66b07761b9d3b793da0d69fdab34eb
sharafadeen hammed <sharafadeenhammed@yahoo.com>
2026-04-14 01:02:29 +0100

    added logger

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import dotenv from 'dotenv';
 import sequelize from './config/database';
 import notificationRoutes from './routes/notificationRoutes';
 import { pollYouTube } from './services/youtubeService';
+import morgan from "morgan"
 
 dotenv.config();
 
@@ -15,6 +16,7 @@ app.use(helmet());
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(morgan("dev"))
 
 // Routes
 app.use('/api/notifications', notificationRoutes);
